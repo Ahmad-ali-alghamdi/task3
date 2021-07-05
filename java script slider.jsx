2e8904e827e7d6f0e92ce061b90f969447a87498
Ahmad-ali-alghamdi <60336128+Ahmad-ali-alghamdi@users.noreply.github.com>
2021-07-05 18:50:04 +0300

    Add files via upload

---
+++ java script slider.jsx
@@ -0,0 +1,14 @@
+function show_value2(x)
+{
+ document.getElementById("slider_value2").innerHTML=x;
+}
+function add_one()
+{
+  document.f.sld6.value=parseInt(document.f.sld6.value)+1;
+  show_value2(document.f.sld6.value);
+}
+function subtract_one()
+{
+  document.f.sld6.value=parseInt(document.f.sld6.value)-1;
+  show_value2(document.f.sld6.value);
+}
